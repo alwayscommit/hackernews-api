224e6f45b6130eedc1bb524f48f0a85c56c6145c
it-by-choice <ranglaniaakash@gmail.com>
2020-06-04 14:53:52 +0530

    loaded spring initializr

---
+++ src/main/java/com/hackernews/api/HackernewsApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hackernews.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackernewsApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackernewsApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackernews/api/HackernewsApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackernews.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackernewsApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
