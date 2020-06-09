14cae7d10e0542a2d0af63fa325afe1e14d20c72
it-by-choice <ranglaniaakash@gmail.com>
2020-06-09 15:51:44 +0530

    test-case not needed

--- src/test/java/com/hackernews/api/HackernewsApiApplicationTests.java
+++ src/test/java/com/hackernews/api/HackernewsApiApplicationTests.java
@@ -22,18 +22,18 @@ class HackernewsApiApplicationTests {
 		assertEquals(5, minutes);
 	}
 	
-	@Test
+/*	@Test
 	void testUnixYear() {
 		LocalDate profileCreationDate = Instant.ofEpochSecond(1397159285L).atZone(ZoneId.systemDefault()).toLocalDate();
 		assertEquals("2014-04-11", profileCreationDate.toString());
 		assertEquals(6, Period.between(profileCreationDate, LocalDate.now()).getYears());
-	}
+	}*/
 	
-	@Test
+/*	@Test
 	void testUnixTime() {
 		LocalDateTime creationTime = Instant.ofEpochSecond(1591455729L).atZone(ZoneId.systemDefault()).toLocalDateTime();
 		System.out.println(creationTime);
-	}
+	}*/
 	
 	
 
